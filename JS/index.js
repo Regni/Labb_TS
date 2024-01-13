2636045f2bb9779f19c46c2fb9a89950e6f439a1
Alexander Kuiper <kuiper9000@gmail.com>
2024-01-13 02:16:23 +0100

    function update need remove wrong code

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,32 @@ let loan = 1000000;
 let year = 0.0025;
 let interest = 300;
 function amount(p, r, n) {
-    let total = p * ((r * (1 + r) ** n) / ((1 + r) ** n - 1));
-    return total;
+    let monthlyPay = p * ((r * (1 + r) ** n) / ((1 + r) ** n - 1));
+    const paidInterest = totalInterest(n, p, monthlyPay);
+    const outputDiv = document.getElementById("output");
+    const newP = document.createElement("p");
+    newP.innerHTML = `You have to pay ${formatMoney(monthlyPay)} per month and the what you paid in interest is ${formatMoney(paidInterest)} so the total is ${formatMoney((paidInterest + p))}`;
+    console.log(`${monthlyPay} and months left ${n} your total is ${p}`);
+    outputDiv.appendChild(newP);
 }
-console.log(amount(loan, year, interest));
+function monthlyPay(loan, interest, months) {
+    return loan * ((interest * (1 + interest) ** months) / ((1 + interest) ** months - 1));
+}
+function totalInterest(months, totalPaid, monthPaid) {
+    return months * monthPaid - totalPaid;
+}
+function formatMoney(unformatted) {
+    return unformatted.toLocaleString("sv-SE", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+}
+const loanInput = document.getElementById("loan");
+loanInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        let test = Number(loanInput.value);
+        console.log(test);
+        //write test logic here
+    }
+});
+let test = Number(loanInput.value);
+console.log(test);
+console.log(monthlyPay(loan, year, interest));
--- TS/index.ts
+++ TS/index.ts
@@ -2,10 +2,42 @@ let loan:number = 1000000
 let year:number = 0.0025
 let interest:number = 300
 
-function amount(p:number,r:number,n:number):number{
-let total:number = p * ((r*(1+r)**n)/((1+r)**n-1))
+function amount(p:number,r:number,n:number):void{
+let monthlyPay:number = p * ((r*(1+r)**n)/((1+r)**n-1))
+const paidInterest = totalInterest(n,p,monthlyPay)
+const outputDiv = document.getElementById("output") as HTMLDivElement
+  const newP = document.createElement("p")
+  newP.innerHTML=`You have to pay ${formatMoney(monthlyPay)} per month and the what you paid in interest is ${formatMoney(paidInterest)} so the total is ${formatMoney((paidInterest + p))}`
+  console.log(`${monthlyPay} and months left ${n} your total is ${p}`)
+  outputDiv.appendChild(newP)
 
-return total
+
 }
 
-console.log(amount(loan,year,interest))
+function monthlyPay(loan:number, interest:number, months:number):number{
+  return loan * ((interest*(1+interest)**months)/((1+interest)**months-1))
+}
+
+function totalInterest(months:number,totalPaid:number,monthPaid:number):number{
+  return months*monthPaid-totalPaid
+}
+
+function formatMoney(unformatted:number):string{
+  return unformatted.toLocaleString("sv-SE",{minimumFractionDigits:2, maximumFractionDigits: 2})
+}
+
+const loanInput = document.getElementById("loan") as HTMLInputElement
+loanInput.addEventListener("keydown",(e)=>{
+  if(e.key == "Enter"){
+    e.preventDefault()
+    let test:number = Number(loanInput.value)
+console.log(test)
+
+    //write test logic here
+  }
+})
+let test:number = Number(loanInput.value)
+console.log(test)
+
+
+console.log(monthlyPay(loan,year,interest))
